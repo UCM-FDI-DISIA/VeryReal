8e933290020e8d76bbd73cc17d642e5c910d9050
CiscoGalvan <fragalva@ucm.es>
2024-02-21 13:59:42 +0100

    Metido Ogre en el proyecto
    Realización del CMake de Ogre quitando y poniendo las opciones que nos interesan
    
    Co-Authored-By: CristinaMora <100274708+CristinaMora@users.noreply.github.com>

---
+++ VeryRealSolucion/dependencies/Ogre/src/CMakeFiles/Export/df49adab93b9e0c10c64f72458b31971/OgreTargets-debug.cmake
@@ -0,0 +1,169 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "OgreMain" for configuration "Debug"
+set_property(TARGET OgreMain APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreMain PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreMain_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreMain_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreMain )
+list(APPEND _cmake_import_check_files_for_OgreMain "${_IMPORT_PREFIX}/lib/OgreMain_d.lib" "${_IMPORT_PREFIX}/bin/OgreMain_d.dll" )
+
+# Import target "RenderSystem_GL" for configuration "Debug"
+set_property(TARGET RenderSystem_GL APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(RenderSystem_GL PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/RenderSystem_GL_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/RenderSystem_GL_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets RenderSystem_GL )
+list(APPEND _cmake_import_check_files_for_RenderSystem_GL "${_IMPORT_PREFIX}/bin/RenderSystem_GL_d.lib" "${_IMPORT_PREFIX}/bin/RenderSystem_GL_d.dll" )
+
+# Import target "Plugin_OctreeSceneManager" for configuration "Debug"
+set_property(TARGET Plugin_OctreeSceneManager APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Plugin_OctreeSceneManager PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_OctreeSceneManager_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_OctreeSceneManager_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Plugin_OctreeSceneManager )
+list(APPEND _cmake_import_check_files_for_Plugin_OctreeSceneManager "${_IMPORT_PREFIX}/bin/Plugin_OctreeSceneManager_d.lib" "${_IMPORT_PREFIX}/bin/Plugin_OctreeSceneManager_d.dll" )
+
+# Import target "Plugin_BSPSceneManager" for configuration "Debug"
+set_property(TARGET Plugin_BSPSceneManager APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Plugin_BSPSceneManager PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_BSPSceneManager_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_BSPSceneManager_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Plugin_BSPSceneManager )
+list(APPEND _cmake_import_check_files_for_Plugin_BSPSceneManager "${_IMPORT_PREFIX}/bin/Plugin_BSPSceneManager_d.lib" "${_IMPORT_PREFIX}/bin/Plugin_BSPSceneManager_d.dll" )
+
+# Import target "Codec_STBI" for configuration "Debug"
+set_property(TARGET Codec_STBI APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Codec_STBI PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Codec_STBI_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Codec_STBI_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Codec_STBI )
+list(APPEND _cmake_import_check_files_for_Codec_STBI "${_IMPORT_PREFIX}/bin/Codec_STBI_d.lib" "${_IMPORT_PREFIX}/bin/Codec_STBI_d.dll" )
+
+# Import target "Plugin_ParticleFX" for configuration "Debug"
+set_property(TARGET Plugin_ParticleFX APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Plugin_ParticleFX PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_ParticleFX_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_ParticleFX_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Plugin_ParticleFX )
+list(APPEND _cmake_import_check_files_for_Plugin_ParticleFX "${_IMPORT_PREFIX}/bin/Plugin_ParticleFX_d.lib" "${_IMPORT_PREFIX}/bin/Plugin_ParticleFX_d.dll" )
+
+# Import target "Plugin_PCZSceneManager" for configuration "Debug"
+set_property(TARGET Plugin_PCZSceneManager APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Plugin_PCZSceneManager PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_PCZSceneManager_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_PCZSceneManager_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Plugin_PCZSceneManager )
+list(APPEND _cmake_import_check_files_for_Plugin_PCZSceneManager "${_IMPORT_PREFIX}/bin/Plugin_PCZSceneManager_d.lib" "${_IMPORT_PREFIX}/bin/Plugin_PCZSceneManager_d.dll" )
+
+# Import target "Plugin_OctreeZone" for configuration "Debug"
+set_property(TARGET Plugin_OctreeZone APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Plugin_OctreeZone PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_OctreeZone_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Plugin_OctreeZone_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Plugin_OctreeZone )
+list(APPEND _cmake_import_check_files_for_Plugin_OctreeZone "${_IMPORT_PREFIX}/bin/Plugin_OctreeZone_d.lib" "${_IMPORT_PREFIX}/bin/Plugin_OctreeZone_d.dll" )
+
+# Import target "Codec_Assimp" for configuration "Debug"
+set_property(TARGET Codec_Assimp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(Codec_Assimp PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/bin/Codec_Assimp_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Codec_Assimp_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets Codec_Assimp )
+list(APPEND _cmake_import_check_files_for_Codec_Assimp "${_IMPORT_PREFIX}/bin/Codec_Assimp_d.lib" "${_IMPORT_PREFIX}/bin/Codec_Assimp_d.dll" )
+
+# Import target "OgrePaging" for configuration "Debug"
+set_property(TARGET OgrePaging APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgrePaging PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgrePaging_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgrePaging_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgrePaging )
+list(APPEND _cmake_import_check_files_for_OgrePaging "${_IMPORT_PREFIX}/lib/OgrePaging_d.lib" "${_IMPORT_PREFIX}/bin/OgrePaging_d.dll" )
+
+# Import target "OgreMeshLodGenerator" for configuration "Debug"
+set_property(TARGET OgreMeshLodGenerator APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreMeshLodGenerator PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreMeshLodGenerator_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreMeshLodGenerator_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreMeshLodGenerator )
+list(APPEND _cmake_import_check_files_for_OgreMeshLodGenerator "${_IMPORT_PREFIX}/lib/OgreMeshLodGenerator_d.lib" "${_IMPORT_PREFIX}/bin/OgreMeshLodGenerator_d.dll" )
+
+# Import target "OgreProperty" for configuration "Debug"
+set_property(TARGET OgreProperty APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreProperty PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreProperty_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreProperty_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreProperty )
+list(APPEND _cmake_import_check_files_for_OgreProperty "${_IMPORT_PREFIX}/lib/OgreProperty_d.lib" "${_IMPORT_PREFIX}/bin/OgreProperty_d.dll" )
+
+# Import target "OgreTerrain" for configuration "Debug"
+set_property(TARGET OgreTerrain APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreTerrain PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreTerrain_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreTerrain_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreTerrain )
+list(APPEND _cmake_import_check_files_for_OgreTerrain "${_IMPORT_PREFIX}/lib/OgreTerrain_d.lib" "${_IMPORT_PREFIX}/bin/OgreTerrain_d.dll" )
+
+# Import target "OgreRTShaderSystem" for configuration "Debug"
+set_property(TARGET OgreRTShaderSystem APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreRTShaderSystem PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreRTShaderSystem_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreRTShaderSystem_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreRTShaderSystem )
+list(APPEND _cmake_import_check_files_for_OgreRTShaderSystem "${_IMPORT_PREFIX}/lib/OgreRTShaderSystem_d.lib" "${_IMPORT_PREFIX}/bin/OgreRTShaderSystem_d.dll" )
+
+# Import target "OgreVolume" for configuration "Debug"
+set_property(TARGET OgreVolume APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreVolume PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreVolume_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreVolume_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreVolume )
+list(APPEND _cmake_import_check_files_for_OgreVolume "${_IMPORT_PREFIX}/lib/OgreVolume_d.lib" "${_IMPORT_PREFIX}/bin/OgreVolume_d.dll" )
+
+# Import target "OgreOverlay" for configuration "Debug"
+set_property(TARGET OgreOverlay APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OgreOverlay PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OgreOverlay_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OgreOverlay_d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OgreOverlay )
+list(APPEND _cmake_import_check_files_for_OgreOverlay "${_IMPORT_PREFIX}/lib/OgreOverlay_d.lib" "${_IMPORT_PREFIX}/bin/OgreOverlay_d.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
