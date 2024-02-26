9fe531ec6fc5022cb596b8641ecc63abe382624e
sheilajulvez <sjulvez@ucm.es>
2024-02-26 19:29:35 +0100

    Singelton y Vector 2
    Co-Authored-By: PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>

---
+++ VeryRealSolucion/src/GuilleElArquitecto/Vector2.cpp
@@ -0,0 +1,39 @@
+#include "Vector2.h"
+Vector2::Vector2() {
+	x = 0;
+	y = 0;
+}
+Vector2::Vector2(float x, float y) {
+	this->x = x;
+	this->y= y;
+
+}
+Vector2::Vector2(const Vector2& vector) {
+	x = vector.x;
+	y = vector.y;
+}
+
+Vector2::~Vector2() {
+
+}
+
+Vector2 Vector2::operator+(const Vector2& vector) {
+	return {x + vector.x,y + vector.y };
+}
+Vector2 Vector2::operator-(const Vector2& vector) {
+	return { x - vector.x,y - vector.y };
+}
+Vector2 Vector2::operator*(const float k) {
+	return{ x * k,y * k };
+}
+Vector2 Vector2::operator/(const float k) {
+	return{ x / k,y / k };
+}
+Vector2 Vector2::operator=(const Vector2& vector) {
+	return{ x = vector.x,y = vector.y };
+}
+//para hacer Cout en la consola, ayudarnos a depurar
+std::ostream& operator<<(std::ostream& o, const Vector2& vector) {
+	o << vector.GetX() << " , " << vector.GetY() << "\n";
+	return o;
+}
---
+++ VeryRealSolucion/src/GuilleElArquitecto/Vector2.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+class Vector2
+{
+private: 
+	float x;
+	float y;
+public:
+	Vector2();
+	Vector2(float x, float y);
+	Vector2(const Vector2& vector);
+	
+	virtual ~Vector2();
+
+	inline void SetX(float x) { this->x = x; }
+	inline void SetY(float y) { this->y = y; }
+	inline void SetVector(float x, float y) { this->x = x; this->y = y; }
+	inline void SetVector(const Vector2& vector) { x = vector.x; y = vector.y; }
+
+	inline float GetX()const { return x; }
+	inline float GetY()const { return y; }
+	inline Vector2 GetVector() { return { x,y }; }
+
+	Vector2 operator+(const Vector2& vector);
+	Vector2 operator-(const Vector2& vector);
+	Vector2 operator*(const float k);
+	Vector2 operator/(const float k);
+	Vector2 operator=(const Vector2& vector);
+	
+	//devuelve el modulo
+	inline float Magnitude() { return sqrtf(powf(x, 2) + powf(y, 2)); }
+	//normaliza el vector
+	inline Vector2 Normalize() { return *this / Magnitude(); }
+};
+std::ostream& operator<<(std::ostream& o, const Vector2& vec);
+
