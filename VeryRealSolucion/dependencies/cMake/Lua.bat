b552b698243015c3b46d9b354121735dbe558e36
AgusCDT <acastrod@ucm.es>
2024-02-29 21:18:51 +0100

    Copia de dll Lua en bin

--- VeryRealSolucion/dependencies/cMake/Lua.bat
+++ VeryRealSolucion/dependencies/cMake/Lua.bat
@@ -5,7 +5,7 @@ set BAT_DIR=%~dp0
 set LUA_SRC=%BAT_DIR%..\Lua
 set LUA_BUILD=%BAT_DIR%..\Lua\Builds\
 set LUA_SLN=%BAT_DIR%..\Lua\Builds\Lua.sln
-::set DLL_FOLDERS=%BAT_DIR%..\..\bin
+set DLL_FOLDERS=%BAT_DIR%..\..\bin
 
 cmake ^
 -S %LUA_SRC% -B %LUA_BUILD%
@@ -16,9 +16,9 @@ rem Compilacion de la solucion en Debug y en Release
 msbuild %LUA_SLN% /t:build /p:configuration=Debug /maxcpucount
 msbuild %LUA_SLN% /t:build /p:configuration=Release /maxcpucount
 
-::XCOPY /y /s .\bin\Debug\lua_d.dll %DLL_FOLDERS%\LuaLuengo\Debug\
-   ::    echo %DLL_FOLDERS%
-    ::XCOPY /y /s .\bin\Release\lua.dll %DLL_FOLDERS%\LuaLuengo\Release\
-    ::   echo %DLL_FOLDERS%
+::XCOPY /y /s .\Debug\lua_d.dll %DLL_FOLDERS%
+::    echo %DLL_FOLDERS%
+XCOPY /y /s .\Release\lua.dll %DLL_FOLDERS%
+    echo %DLL_FOLDERS%
 echo Terminada la build de Lua
 pause
