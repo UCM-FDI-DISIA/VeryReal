5545a0cf547b2007b9d9149e9c45e94160c0568b
AgusCDT <acastrod@ucm.es>
2024-03-10 21:10:22 +0100

    ScriptManager v1
    - Creación del script que leerá y creará las entidades y sus componentes dentro de una escena.
    - Falta principalmente generalizar la creación de los componentes

---
+++ VeryRealSolucion/src/LuaLuengo/ScriptManager.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef _SCRIPT_MANAGER
+#define _SCRIPT_MANAGER
+
+#include "Singleton.h"
+#include "Manager.h"
+
+struct lua_State;
+
+using namespace std;
+class ScriptManager : public VeryReal::Manager<ScriptManager> {
+	friend Singleton<ScriptManager>;
+
+private:
+	lua_State* lua_state;
+
+public:
+	ScriptManager();
+	~ScriptManager();
+
+	// Abre el archivo .lua
+	void Init();
+	// Compreueba error al abrir archivo
+	void Error(int status);
+	// Método para probar la creación de entidades y componentes mediante Lua
+	void Test();
+};
+
+// Instancia Singleton
+// @return un puntero de la instancia
+inline ScriptManager& SM() {
+	return *ScriptManager::Instance();
+}
+#endif // !_SCRIPT_MANAGER
