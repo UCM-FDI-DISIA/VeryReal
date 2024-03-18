a49ce93343d64a082462efd97b382a61fc8b5e41
JoseMorenob <jmoren13@ucm.es>
2024-03-18 19:56:12 +0100

    .clang ejecutadoi en romerender

--- VeryRealSolucion/src/RomeRender/RenderManager.cpp
+++ VeryRealSolucion/src/RomeRender/RenderManager.cpp
@@ -12,73 +12,66 @@
 #include <OgreVector3.h>
 #include "SGTechniqueResolverListener.h"
 //mehrender, camara y eso
-VeryReal::RenderManager::RenderManager():window(nullptr),root(nullptr), scene_manager(nullptr), render_system(nullptr), viewport(nullptr), filesystem_layer(nullptr){
-    
-}
+VeryReal::RenderManager::RenderManager()
+    : window(nullptr), root(nullptr), scene_manager(nullptr), render_system(nullptr), viewport(nullptr), filesystem_layer(nullptr) { }
 VeryReal::RenderManager::~RenderManager() {
 
     if (root == nullptr) {
-        #ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
         cerr << DEBUG_ROOT_ERROR;
-        #endif
-        return; }
+#endif
+        return;
+    }
     //vuelta a la escena por defecto
     root->destroySceneManager(scene_manager);
     Ogre::MaterialManager::getSingleton().setActiveScheme(Ogre::MaterialManager::DEFAULT_SCHEME_NAME);
 
     UnloadShaders();
-    delete(window);
+    delete (window);
     window = nullptr;
     delete root;
     root = nullptr;
     delete filesystem_layer;
     filesystem_layer = nullptr;
-    
 }
 void VeryReal::RenderManager::InitManager(std::string const& name) {
     appname = name;
-	filesystem_layer = new Ogre::FileSystemLayer(appname);
+    filesystem_layer = new Ogre::FileSystemLayer(appname);
     Ogre::String pluginsPath;
     pluginsPath = filesystem_layer->getConfigFilePath("plugins.cfg");
-    if (!Ogre::FileSystemLayer::fileExists(pluginsPath))
-    {
-        #ifdef DEBUG_MODE
+    if (!Ogre::FileSystemLayer::fileExists(pluginsPath)) {
+#ifdef DEBUG_MODE
         cerr << DEBUG_PLUGINS_ERROR << pluginsPath << "\n";
-        #endif
-      
+#endif
     }
     //lo pongo asi y no la rita porque en teoria va a estar al lado del .exe
     Ogre::String ogrepath = filesystem_layer->getConfigFilePath("ogre.cfg");
     if (!Ogre::FileSystemLayer::fileExists(ogrepath)) {
-        #ifdef DEBUG_MODE
-                cerr << DEBUG_PLUGINS_ERROR << ogrepath << "\n";
-        #endif
+#ifdef DEBUG_MODE
+        cerr << DEBUG_PLUGINS_ERROR << ogrepath << "\n";
+#endif
     }
-    root= new Ogre::Root(pluginsPath, ogrepath);
-    if (!root->restoreConfig())
-    {
-         root->showConfigDialog(nullptr);
+    root = new Ogre::Root(pluginsPath, ogrepath);
+    if (!root->restoreConfig()) {
+        root->showConfigDialog(nullptr);
     }
     //inicializamos root pero sin ventana
     root->initialise(false);
-   
+
     const Ogre::RenderSystemList renderSystems = root->getAvailableRenderers();
     render_system = renderSystems.front();
     root->setRenderSystem(render_system);
 
-   
 
     //inicializamos sdl
-    if (SDL_WasInit(SDL_INIT_VIDEO))SDL_Init(SDL_INIT_VIDEO);
-    #ifdef ROME_RENDER_TEST
-    SDL_Init(SDL_INIT_EVERYTHING);//mientras no esta todo bien
-    #endif
-
-   
+    if (SDL_WasInit(SDL_INIT_VIDEO)) SDL_Init(SDL_INIT_VIDEO);
+#ifdef ROME_RENDER_TEST
+    SDL_Init(SDL_INIT_EVERYTHING);   //mientras no esta todo bien
+#endif
 
 
     scene_manager = root->createSceneManager();
-    render_system->_initRenderTargets();//mira esto ni idea que hace
+    render_system->_initRenderTargets();   //mira esto ni idea que hace
 
 
     window = new VeryReal::Window();
@@ -86,13 +79,11 @@ void VeryReal::RenderManager::InitManager(std::string const& name) {
         window->CreateWindoww();
     }
     else {
-        #ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
         cerr << DEBUG_WINDOW_ERROR;
-        #endif
-
+#endif
     }
-    
- 
+
 
     //root->startRendering();
 
@@ -101,43 +92,34 @@ void VeryReal::RenderManager::InitManager(std::string const& name) {
     std::string arch_name;
     Ogre::String ogrepath2 = filesystem_layer->getConfigFilePath("resources.cfg");
     if (!Ogre::FileSystemLayer::fileExists(ogrepath2)) {
-        #ifdef DEBUG_MODE
-                cerr << DEBUG_RESOURCES_ERROR << ogrepath << "\n";
-        #endif
+#ifdef DEBUG_MODE
+        cerr << DEBUG_RESOURCES_ERROR << ogrepath << "\n";
+#endif
     }
-    Ogre::ConfigFile cf;
+    Ogre::ConfigFile cf;    
     cf.load(ogrepath2);
 
     Ogre::ConfigFile::SettingsBySection_ seci = cf.getSettingsBySection();
-    for (auto i = seci.begin(); i != seci.end(); i++)
-    {
+    for (auto i = seci.begin(); i != seci.end(); i++) {
         sec_name = i->first;
         Ogre::ConfigFile::SettingsMultiMap settings = i->second;
         Ogre::ConfigFile::SettingsMultiMap::iterator mmi;
-        for (mmi = settings.begin(); mmi != settings.end(); ++mmi)
-        {
+        for (mmi = settings.begin(); mmi != settings.end(); ++mmi) {
             type_name = mmi->first;
             arch_name = mmi->second;
-            Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-                ogrepath2+arch_name, type_name);
+            Ogre::ResourceGroupManager::getSingleton().addResourceLocation(ogrepath2 + arch_name, type_name);
         }
     }
-   
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 }
 
 
-
-   
-
- 
-
 void VeryReal::RenderManager::LoadResources() {
 
-    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("./assets", "FileSystem", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, true);
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation("./assets", "FileSystem", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                                                                   true);
     Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-
 }
 void VeryReal::RenderManager::LoadShaders() {
     if (Ogre::RTShader::ShaderGenerator::initialize()) {
@@ -150,50 +132,41 @@ void VeryReal::RenderManager::LoadShaders() {
     }
 }
 void VeryReal::RenderManager::UnloadShaders() {
-   
-    if (material_listener != nullptr)
-    {
-      //  Ogre::MaterialManager::getSingleton().removeListener(material_listener);
+
+    if (material_listener != nullptr) {
+        //  Ogre::MaterialManager::getSingleton().removeListener(material_listener);
         delete material_listener;
         material_listener = nullptr;
     }
 
     // Destroy RTShader system.
-    if (shader_generator!= nullptr)
-    {
-       
+    if (shader_generator != nullptr) {
+
         Ogre::RTShader::ShaderGenerator::destroy();
         shader_generator = nullptr;
     }
 }
-Ogre::RenderWindow* VeryReal::RenderManager::GetRenderWindow() {
-    return window->GetOgreWindow();
-}
-Ogre::SceneManager* VeryReal::RenderManager::SceneManagerOgree() {
-    return scene_manager;
-}
-void VeryReal::RenderManager::Update(const double& dt) {
-    root->renderOneFrame();
-   
- }
+Ogre::RenderWindow* VeryReal::RenderManager::GetRenderWindow() { return window->GetOgreWindow(); }
+Ogre::SceneManager* VeryReal::RenderManager::SceneManagerOgree() { return scene_manager; }
+void VeryReal::RenderManager::Update(const double& dt) { root->renderOneFrame(); }
 
 Ogre::SceneNode* VeryReal::RenderManager::CreateNode(VeryReal::Vector3 vec) {
- 
+
     //DA ERROR DE AMBIUEGAD CON VECTOR3 NUESTRO Y VECTOR3 OGRE NO SE
     Ogre::SceneNode* node = scene_manager->getRootSceneNode()->createChildSceneNode();
-    //AQUI VA EL SET POS 
-    node->setPosition({ vec.GetX(),vec.GetY(),vec.GetZ() });
+    //AQUI VA EL SET POS
+    node->setPosition({vec.GetX(), vec.GetY(), vec.GetZ()});
     return node;
 }
 Ogre::SceneNode* VeryReal::RenderManager::CreateChildSceneNode(Ogre::SceneNode* nod, VeryReal::Vector3 vec) {
     Ogre::SceneNode* node = scene_manager->getRootSceneNode()->createChildSceneNode();
-    //AQUI VA EL SET POS 
-    node->setPosition({ vec.GetX(),vec.GetY(),vec.GetZ() });
+    //AQUI VA EL SET POS
+    node->setPosition({vec.GetX(), vec.GetY(), vec.GetZ()});
     return node;
 }
 void VeryReal::RenderManager::DeleteNode(Ogre::SceneNode* nod) {
-    
+
     scene_manager->destroySceneNode(nod);
 
-   // delete(nod);
+    // delete(nod);
 }
