8b729041f8fbc3a1eceb92918deac01f58a6d21a
CristinaMora <Crmora03@ucm.es>
2024-04-15 10:21:16 +0200

    creator
    porque aparece el nombre de mi her?

---
+++ VeryRealSolucion/src/RomeRender/UI/CreatorUIProgressBarComponent.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef CREATORUIPROGRESSBARCOMPONENT
+#define CREATORUIPROGRESSBARCOMPONENT
+#include "CreatorComponent.h"
+namespace VeryReal {
+class VERYREAL_API CreatorUIProgressBarComponent : public CreatorComponent {
+        public:
+    CreatorUIProgressBarComponent(){};
+    virtual ~CreatorUIProgressBarComponent(){};
+    Component* CreatorSpecificComponent() override;
+    void SpecificInitComponent(Component* c) override;
+}; 
+};
+#endif
