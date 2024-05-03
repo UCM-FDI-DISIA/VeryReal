48b0b2ad5642c8c648512a6ba9e1d56684678896
PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>
2024-05-04 00:04:34 +0200

    Eliminacion de escenas arreglada
    Co-Authored-By: AgusCDT <100000318+AgusCDT@users.noreply.github.com>

--- VeryRealSolucion/src/GuilleElArquitecto/SceneManager.cpp
+++ VeryRealSolucion/src/GuilleElArquitecto/SceneManager.cpp
@@ -2,75 +2,82 @@
 #include "Scene.h"
 using namespace std;
 
-VeryReal::SceneManager::~SceneManager(){ 
-	
-	for (auto it : scenes_list) {
-        delete (it);
-	}
+VeryReal::SceneManager::~SceneManager() {
 
+    for (auto it : scenes_list) {
+        delete (it);
+    }
 };
 
 
 void VeryReal::SceneManager::Update(const double& dt) {
-	for (auto it : scenes_list) if (it->GetActive())it->Update(dt);
-	Refresh();
-	
+    for (auto it : scenes_list)
+        if (it->GetActive()) it->Update(dt);
+    Refresh();
 }
 
 void VeryReal::SceneManager::Refresh() {
-	for (auto it : scenes_list)if (it->GetToEliminate())RemoveScene(it->GetName());
-}
+    /*  for (auto it : scenes_list)
+        if (it->GetToEliminate()) {
+            RemoveScene(it);
+        }*/
 
-VeryReal::Scene* VeryReal::SceneManager::AddScene(scene_name name,bool active) {
-	RemoveScene(name);
-	Scene* scene = new Scene();
-	scene->SetName(name);
-	scenes_list.push_back(scene);
-	ActivationScene(name, active);
-	EliminationScene(name, false);
-
-	return scene;
-}
-
-void VeryReal::SceneManager::RemoveScene(scene_name name) {
-	for (auto it : scenes_list) {
-        if (it->GetName() == name) {
-            it->SetToEliminate(true);
-            it->SetActive(false);
-            scenes_list.remove(it);
+    auto it = scenes_list.begin();
+    while (it != scenes_list.end()) {
+        if ((*it)->GetToEliminate()) {
+            it = RemoveScene(it);
         }
-	}
+        else {
+            ++it;
+        }
+    }
+}
+
+VeryReal::Scene* VeryReal::SceneManager::AddScene(scene_name name, bool active) {
+
+
+    Scene* scene = new Scene();
+    scene->SetName(name);
+    scenes_list.push_back(scene);
+    ActivationScene(name, active);
+    EliminationScene(name, false);
+
+    return scene;
+}
+
+std::list<VeryReal::Scene*>::iterator VeryReal::SceneManager::RemoveScene(std::list<VeryReal::Scene*>::iterator scene) {
+    delete (*scene);
+    return scenes_list.erase(scene);
 }
 
 void VeryReal::SceneManager::ActivationScene(scene_name name, bool active) {
-	for (auto it : scenes_list) if (it->GetName() == name)it->SetActive(active);
+    for (auto it : scenes_list)
+        if (it->GetName() == name) it->SetActive(active);
 }
-
-/*void DesactivateScene(scene_name name) {
-	for (auto it : scenes_list) if (it->GetName() == name)it->SetActive(false);
-}*/
 
 void VeryReal::SceneManager::EliminationScene(scene_name name, bool to_eliminate) {
-	for (auto it : scenes_list) if (it->GetName() == name)it->SetToEliminate(to_eliminate);
+    for (auto it : scenes_list)
+        if (it->GetName() == name) it->SetToEliminate(to_eliminate);
 }
 
-VeryReal::Scene* VeryReal::SceneManager::GetScene(scene_name name) { 
-	if (HasScene(name)) {
+VeryReal::Scene* VeryReal::SceneManager::GetScene(scene_name name) {
+    if (HasScene(name)) {
         for (auto c : scenes_list)
-            if (c->GetName() == name) 
-				return c;
+            if (c->GetName() == name) return c;
 
         return nullptr;
-	}
-	else 
-		return nullptr;
+    }
+    else
+        return nullptr;
 }
 
 bool VeryReal::SceneManager::HasScene(scene_name name) {
-	for (auto c : scenes_list) if (c->GetName() == name)return true;
-	return false;
+    for (auto c : scenes_list)
+        if (c->GetName() == name) return true;
+    return false;
 }
 
 VeryReal::Scene* VeryReal::SceneManager::GetActiveScene() {
-	for (auto it : scenes_list) if (it->GetActive()) return it;
+    for (auto it : scenes_list)
+        if (it->GetActive()) return it;
 }
--- VeryRealSolucion/src/GuilleElArquitecto/SceneManager.h
+++ VeryRealSolucion/src/GuilleElArquitecto/SceneManager.h
@@ -10,42 +10,40 @@ using scene_name = std::string;
 #pragma warning(disable : 4251)
 
 namespace VeryReal {
-	class Scene;
-	class VERYREAL_GUILLEELARQUITECTO SceneManager : public VeryReal::Manager<SceneManager> {
-	private:
-		//lista con todas la escenas
-		std::list<VeryReal::Scene*> scenes_list;
+class Scene;
+class VERYREAL_GUILLEELARQUITECTO SceneManager : public VeryReal::Manager<SceneManager> {
+        private:
+    //lista con todas la escenas
+    std::list<VeryReal::Scene*> scenes_list;
     SceneManager(){};
-  
-	public:
-  
-    static bool Init() { 
-		SceneManager* a = new SceneManager();
+
+        public:
+    static bool Init() {
+        SceneManager* a = new SceneManager();
         if (a != nullptr) {
-                    instance_pointer.reset(a);
-					return true;
-		}
+            instance_pointer.reset(a);
+            return true;
+        }
         return false;
+    }
+    virtual ~SceneManager();
 
-	}
-		virtual ~SceneManager();
+    void Update(const double& dt);
+    //si hay escenas que quieren ser eliminadas las removemos aquí
+    //esto elimina de manera más segura las escenas
+    void Refresh();
 
-		void Update(const double& dt);
-		//si hay escenas que quieren ser eliminadas las removemos aquí
-		//esto elimina de manera más segura las escenas
-		void Refresh();
+    VeryReal::Scene* AddScene(scene_name name, bool active);
+    std::list<VeryReal::Scene*>::iterator RemoveScene(std::list<VeryReal::Scene*>::iterator scene);
 
-		VeryReal::Scene* AddScene(scene_name name,bool active);
-		void RemoveScene(scene_name name);
+    void ActivationScene(scene_name name, bool active);
+    void EliminationScene(scene_name name, bool to_eliminate);
+    //si existe la escen la devuleve si no devuelve nullptr
+    Scene* GetScene(scene_name name);
+    bool HasScene(scene_name name);
 
-		void ActivationScene(scene_name name, bool active);
-		void EliminationScene(scene_name name, bool to_eliminate);
-		//si existe la escen la devuleve si no devuelve nullptr
-		Scene* GetScene(scene_name name);
-		bool HasScene(scene_name name);
-
-		Scene* GetActiveScene();
-	};
+    Scene* GetActiveScene();
+};
 }
 
 #pragma warning(default : 4251)
