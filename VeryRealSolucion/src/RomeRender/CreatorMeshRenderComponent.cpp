9d9463d91350ac4ab3583065414e9d648c189de8
PabloM <pabmar31@ucm.es>
2024-05-05 17:14:38 +0200

    parche renderizado prefabs

--- VeryRealSolucion/src/RomeRender/CreatorMeshRenderComponent.cpp
+++ VeryRealSolucion/src/RomeRender/CreatorMeshRenderComponent.cpp
@@ -100,7 +100,7 @@ void VeryReal::CreatorMeshRenderComponent::SpecificInitComponentByCopy(VeryReal:
     MeshRenderComponent* a = static_cast<MeshRenderComponent*>(c);
     MeshRenderComponent* copia = static_cast<MeshRenderComponent*>(other);
 
-    bool isstatic = copia->getStatic();
+    bool isstatic = false;
     std::string modelname = copia->getMeshName(), entityname = copia->getNameEntity(), materialname = copia->getMaterialName();
 
     bool b = a->InitComponent(isstatic, modelname, entityname, materialname);
