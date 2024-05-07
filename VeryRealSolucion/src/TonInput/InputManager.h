cf192861d0a28910a179b102cce61cbf164c664a
CiscoGalvan <fragalva@ucm.es>
2024-05-07 14:40:50 +0200

    Update InputManager.h

--- VeryRealSolucion/src/TonInput/InputManager.h
+++ VeryRealSolucion/src/TonInput/InputManager.h
@@ -33,9 +33,6 @@ class VERYREAL_TONINPUT InputManager : public VeryReal::Manager<InputManager> {
         // Inicializa SDL
      virtual std::pair<bool,std::string> InitManager();
 
-    /// Inicializa SDL
-    virtual std::pair<bool, std::string> InitManager();
-
     /// Limpia el estado de la entrada
     void ClearState(bool clearMouseButtons = false);
 
