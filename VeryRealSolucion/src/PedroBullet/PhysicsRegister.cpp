cbdf1f97cfea909f989672b2afd249255e46d430
CiscoGalvan <fragalva@ucm.es>
2024-05-09 22:58:11 +0200

    sobra

--- VeryRealSolucion/src/PedroBullet/PhysicsRegister.cpp
+++
@@ -1,6 +0,0 @@
-//#include "PhysicsRegister.h"
-//#include "PhysicsManager.h"   // Incluir aquí para evitar circularidad en los headers
-//
-//namespace VeryReal {
-//void PhysicsRegister::RegisterRigidBody(btRigidBody* body) { PhysicsManager::Instance()->AddRigidBody(body); }
-//}
